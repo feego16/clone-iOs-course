6ee2e20409f9ffc03c960b6c2e12ffe4309893f0
orlando grant <feego@aaa-aaaaaaas-MacBook.local>
2016-06-12 21:48:17 -0700

    added files to gitproject supercool ios

--- gitproject/ViewController.swift
+++ gitproject/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var redfish: UIImageView!
 
+    @IBOutlet weak var bluefish: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clickred(sender: AnyObject) {
+        redfish.hidden = true
+    }
 
+    @IBAction func clickbutton(sender: AnyObject) {
+        bluefish.hidden = true
+    }
+    @IBAction func resetbothclors(sender: AnyObject) {
+        redfish.hidden = false
+        bluefish.hidden = false
+    }
 }
 
